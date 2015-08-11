604a8deb4e50a86d45c914af9bb9f24ea7069752
Yu Haidong <yuhaidongyhd@gmail.com>
2015-08-11 10:48:49 +0800

    Activate livereload

--- config.rb
+++ config.rb
@@ -38,9 +38,9 @@ require "slim"
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+configure :development do
+  activate :livereload
+end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
